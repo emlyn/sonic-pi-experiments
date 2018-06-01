571158535367f24e8354ddc91e00116f718450f8
Emlyn Corrin <Emlyn.Corrin@microsoft.com>
2018-06-01 12:52:09 +0100

    Use with_bpm to avoid messing with global state

--- helpers/mml.rb
+++ helpers/mml.rb
@@ -3,53 +3,54 @@
 define :mml do |txt, vol_scale=100|
   octave = 5
   length = 4
-  use_bpm 120
   txt = txt.downcase.gsub(/^mml@| |;$/, '').gsub(/#/, '+').gsub(/w/, 'r')
-  while not txt.empty?
-    r = txt.scan(/^(.)([+-]?)(\d*)(\.?)(.*)$/)
-    cmd, opt, num, dot, txt = r[0]
-    #print cmd, opt, num
-    case cmd
-    when 'o'
-      if not num.empty? then
-        octave = num.to_i
-      end
-    when 'l'
-      if not num.empty? then
-        length = num.to_i
-      end
-    when 't'
-      if not num.empty? then
-        use_bpm num.to_i
-      end
-    when 'v'
-      if not num.empty? then
-        set_volume! num.to_f / vol_scale
-      end
-    when '>'
-      octave += 1
-    when '<'
-      octave -= 1
-    when 'a'..'g', 'r'
-      a = if opt == '+' then 's' elsif opt == '-' then 'b' else '' end
-      n = cmd
-      if n != 'r' then
-        n += a + octave.to_s
-      end
-      t = (if dot.empty? then 4.0 else 6.0 end) / (if num.empty? then length else num.to_i end)
-      r = txt.scan(/^(\^|&[a-gr][+-]?)(\d*)(\.?)(.*)$/)
-      while not r.empty?
-        cmd, num, dot, txt = r[0]
-        t += (if dot.empty? then 4.0 else 6.0 end) / (if num.empty? then length else num.to_i end)
+  with_bpm 120 do
+    until txt.empty?
+      r = txt.scan(/^(.)([+-]?)(\d*)(\.?)(.*)$/)
+      cmd, opt, num, dot, txt = r[0]
+      #print cmd, opt, num
+      case cmd
+      when 'o'
+        if not num.empty? then
+          octave = num.to_i
+        end
+      when 'l'
+        if not num.empty? then
+          length = num.to_i
+        end
+      when 't'
+        if not num.empty? then
+          use_bpm num.to_i
+        end
+      when 'v'
+        if not num.empty? then
+          set_volume! num.to_f / vol_scale
+        end
+      when '>'
+        octave += 1
+      when '<'
+        octave -= 1
+      when 'a'..'g', 'r'
+        a = if opt == '+' then 's' elsif opt == '-' then 'b' else '' end
+        n = cmd
+        if n != 'r' then
+          n += a + octave.to_s
+        end
+        t = (if dot.empty? then 4.0 else 6.0 end) / (if num.empty? then length else num.to_i end)
         r = txt.scan(/^(\^|&[a-gr][+-]?)(\d*)(\.?)(.*)$/)
+        until r.empty?
+          cmd, num, dot, txt = r[0]
+          t += (if dot.empty? then 4.0 else 6.0 end) / (if num.empty? then length else num.to_i end)
+          r = txt.scan(/^(\^|&[a-gr][+-]?)(\d*)(\.?)(.*)$/)
+        end
+        play_pattern_timed [n.to_sym], [t], release: t
+      when 'n'
+        n = num.to_i + 12
+        t = (if dot.empty? then 4.0 else 6.0 end) / length
+        play_pattern_timed [n], [t], release: t
+      else
+        print "Unrecognised:", cmd + opt + num + dot
       end
-      play_pattern_timed [n.to_sym], [t], release: t
-    when 'n'
-      n = num.to_i + 12
-      t = (if dot.empty? then 4.0 else 6.0 end) / length
-      play_pattern_timed [n], [t], release: t
-    else
-      print "Unrecognised:", cmd + opt + num + dot
     end
   end
 end
